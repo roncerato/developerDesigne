cb5fc00d619eaa7361f677ae183b5476270f69ac
Ron <nazarmuhamedow9731@gmail.com>
2024-09-04 09:36:22 +0500

    Imported all components from the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Stack from "@/components/Stack";
 
 export default function Home() {
   return (
-   <></>
+    <main className="main">
+      <Hero/>
+      <Stack/>
+      <Projects/>
+    </main>
   );
 }
